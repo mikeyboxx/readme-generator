1cc8987364b51bb5124a904d7b5dd72132d4e556
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-20 18:30:40 -0400

    Added extra line space to be generated in README file.

---
+++ README_GENR.md
@@ -0,0 +1,58 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# My Daily Almanac
+
+## Description
+
+My Daily Almanac app is a personalized site for a user to jot down his/her thoughts and see various information tailor to their own interests/specifications/needs. Currently the app will show various information such as horoscope, weather, crypto, and drink of the day. As new updates come out user will more choices to view what preference they like to see on their personal page. User will also have a area where they can jot notes and view previous notes saved.
+
+---
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Questions](#questions)
+
+---
+
+## Installation
+
+N/A
+
+---
+
+## Usage
+
+https://mikeyboxx.github.io/my-daily-almanac/
+
+---
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+The MIT License
+
+---
+
+## Contributing
+
+N/A
+
+---
+
+## Tests
+
+N/A
+
+---
+
+## Questions
+
+[Github Profile](https://www.github.com/mikeyboxx)
+
+For any additional questions, you can contact me at happyanddebtfree@gmail.com
+
+---
+
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -6,8 +6,8 @@ const renderTitle = data =>
     `# ${data.title}\n\n`;
 
 const renderDescription = data => 
-    `## Description\n` + 
-    `${data.description}\n---\n\n`;
+    `## Description\n\n` + 
+    `${data.description}\n\n---\n\n`;
     
 const renderTableOfContents = () => 
     `## Table Of Contents\n` + 
@@ -15,33 +15,33 @@ const renderTableOfContents = () =>
     `- [Usage](#usage)\n` + 
     `- [Contributing](#contributing)\n` + 
     `- [License](#license)\n` + 
-    `- [Questions](#questions)\n---\n\n`;
+    `- [Questions](#questions)\n\n---\n\n`;
 
 const renderInstallation = data => 
-    `## Installation\n` + 
-    `${data.installation}\n---\n\n`;
+    `## Installation\n\n` + 
+    `${data.installation}\n\n---\n\n`;
 
 const renderUsage = data => 
-    `## Usage\n` + 
-    `${data.usage}\n---\n\n`;
+    `## Usage\n\n` + 
+    `${data.usage}\n\n---\n\n`;
 
 const renderContributing = data => 
-    `## Contributing\n` + 
-    `${data.contributing}\n---\n\n`;
+    `## Contributing\n\n` + 
+    `${data.contributing}\n\n---\n\n`;
 
 const renderTests = data => 
-    `## Tests\n` + 
-    `${data.tests}\n---\n\n`;
+    `## Tests\n\n` + 
+    `${data.tests}\n\n---\n\n`;
 
 const renderLicense = data => 
-    `## License\n` + 
-    `${licenses[data.licenseIdx].badge}\n\n` + 
+    `## License\n\n` + 
+    `${licenses[data.licenseIdx].badge}\n` + 
     `${licenses[data.licenseIdx].short}\n\n---\n\n`;
 
 const renderQuestions = data => 
-    `## Questions\n` +
+    `## Questions\n\n` +
     `[Github Profile](https://www.github.com/${data.githubUserName})\n\n` + 
-    `For any additional questions, you can contact me at ${data.email}\n\n---`;
+    `For any additional questions, you can contact me at ${data.email}\n\n---\n\n`;
 
 
 const generateMarkdown = data => 
