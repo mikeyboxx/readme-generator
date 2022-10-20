24b7514aaaf7e31bb7d821b7b1f119b63f1d8e63
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-19 21:25:04 -0400

    Added pageSize in promptList array, in order to display more than 5 list choices on a page

--- promptInput.js
+++ promptInput.js
@@ -6,25 +6,25 @@ const inquirer = require('inquirer'); // import the inquirer.js library
 
 // accepts the prompt text to display on the command line, and a flag to make input required 
 const promptInput = (message, required = false) =>
-    inquirer.prompt([{
-        name: "val",
-        type: "input",
-        message: message,
-        validate: input => (input.length > 0 || !required) ||  'This field is required. Please try again...' 
-    }])
-    .then(answer => answer.val); // returns the user input
+inquirer.prompt([{
+    name: "val",
+    type: "input",
+    message: message,
+    validate: input => (input.length > 0 || !required) ||  'This field is required. Please try again...' 
+}])
+.then(answer => answer.val); // returns the user input
 
+
+// accepts the prompt text and an array of question objects, which will display as a list of choices. Returns the index of the choice option.
 const promptList = (message, choices) =>
     inquirer.prompt([{
         name: "val",
         type: "list",
         message: message,
         choices: choices,
-        default: 0,
         pageSize: 100,
-        // validate: input => (input.length > 0 || !required) ||  'This field is required. Please try again...' 
     }])
-    .then(answer => answer.val); // returns the user input
+    .then(answer => answer.val); // index of the License in the licenses array
 
 
 module.exports ={
